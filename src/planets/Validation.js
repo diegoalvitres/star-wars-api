dccecb7df92520a1d0faed34de0d5c22a34f5c21
Diego Alvitres <diegoalvitres@gmail.com>
2023-03-12 11:36:14 -0500

    adding last validations

--- src/planets/Validation.js
+++ src/planets/Validation.js
@@ -12,10 +12,32 @@ async function validateRequestPlanet(payload) {
         nombre: Joi.string().required().min(1),
         peliculas: Joi.array().items(Joi.string()),
         residentes: Joi.array().items(Joi.string()),
-        clima: Joi.string(),
-        terreno: Joi.string() 
+        clima: Joi.string().allow(null),
+        terreno: Joi.string().allow(null),
+        poblacion: Joi.string().allow(null),
+        periodo_actual: Joi.string().allow(null),
+        agua_en_superficie: Joi.string().allow(null),
+        gravedad: Joi.string().allow(null),
+        diametro: Joi.string().allow(null),
+        creado: Joi.string().allow(null).custom(validateCustom, 'validate iso format'),
+        editado: Joi.string().allow(null).custom(validateCustom, 'validate iso format'),
+        url: Joi.string().allow(null)
     });
     await Validator.validate(schema, payload);
+}
+
+function validateCustom(date) {
+    if (!validationDate(date)) {
+        throw new Error("Fecha invalida");
+    }
+}
+
+function validationDate(dateStr) {
+    if (!dateStr) return true;
+    if (!/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.(\d{1,10}))?Z?$/.test(dateStr)) return false;
+    const date = new Date(dateStr);
+    const isValid = !isNaN(date);
+    return isValid;
 }
 
 module.exports = { validateParamName, validateRequestPlanet };
